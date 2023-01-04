f7adbe1e41ddae8afad5c2d39dbd9b549527e2da
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-01-04 21:55:51 +0400

    s

--- src/pages/boxShadow/BoxShadow.tsx
+++ src/pages/boxShadow/BoxShadow.tsx
@@ -81,7 +81,7 @@ const BoxShadow = () => {
   ]
 
   return (
-    <div className=" max-w-6xl  mx-auto pt-16 ">
+    <div className=" max-w-6xl  mx-auto mt-10  mb-20 ">
       <div className="md:flex-row flex flex-col md:justify-center justify-between md:gap-8 gap-16 lg:gap-36 mb-16 ">
         <SideMenu title={`Box shadow config`} resetState={resetState}>
           <div className="p-4 ">
@@ -129,9 +129,7 @@ const BoxShadow = () => {
           <Shape currentValue={currentValue} shadowColor={shadowColor} inset={inset} />
         </div>
       </div>
-      <div className="mb-16">
-        <Output title="CSS rules" renderArray={renderArray} />
-      </div>
+      <Output title="CSS rules" renderArray={renderArray} />
     </div>
   )
 }
