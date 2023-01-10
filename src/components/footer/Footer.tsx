f3b3ba4038f003b67afcaefc62ac99a49acbfbf8
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-01-10 11:53:00 +0400

    2

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div className="bg-primary py-6 h-[120px] ">
+    <div className="bg-primary py-6 h-fit ">
       <div className="flex flex-col text-center gap-y-4 justify-between max-w-5xl mx-auto">
         <div className="">this is a footer</div>
         <div className="">Not optimized for mobile view</div>
