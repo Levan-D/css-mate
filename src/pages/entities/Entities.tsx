39d345d2b011484a4cab4caa7af2fea9a36e0704
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-01-10 19:39:50 +0400

    a

--- src/pages/entities/Entities.tsx
+++ src/pages/entities/Entities.tsx
@@ -8,7 +8,8 @@ const Entities = () => {
   return (
     <div className="my-16 mx-4">
       <h1 className="font-cursiveCustom text-center text-2xl   mb-2">
-        You can find commonly used HTML <span className="text-secondary">entities</span>
+        You can find commonly used HTML
+        <span className="text-secondary">entities&#32;</span>
         below
       </h1>
       <div className="font-cursiveCustom text-center mb-16 text-base">
