85742a20e146421e331b10dc1e9b2141bc21daf5
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-01-10 19:50:42 +0400

    a

--- src/components/wrappers/CatDisplay.tsx
+++ src/components/wrappers/CatDisplay.tsx
@@ -49,13 +49,15 @@ const CatDisplay = ({ Data, Headers, flexBasis }: CatDisplayType) => {
                 {cat.content.map((char, i) => (
                   <div key={i} className={"flex"}>
                     <div className={`${flexBasis}  px-2 `}>{char.character}</div>
-                    <div
-                      className={` ${flexBasis} cursor-pointer   hover:bg-slate-100  active:bg-slate-300 hover:text-dark w-fit px-2 duration-200 rounded-md `}
-                      onClick={() => {
-                        navigator.clipboard.writeText(char.hexadecimal)
-                      }}
-                    >
-                      {char.hexadecimal}
+                    <div className={`${flexBasis} `}>
+                      <div
+                        className={` cursor-pointer   hover:bg-slate-100  active:bg-slate-300 hover:text-dark w-fit px-2 duration-200 rounded-md `}
+                        onClick={() => {
+                          navigator.clipboard.writeText(char.hexadecimal)
+                        }}
+                      >
+                        {char.hexadecimal}
+                      </div>
                     </div>
                     <div className={`${flexBasis}  px-2 `}>{char.description}</div>
                   </div>
