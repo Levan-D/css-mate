9628ca1a66fcfdf07dd1f76e88a3d961ad346e28
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-01-25 17:49:04 +0400

    a

--- src/components/floofGPT/floofGPTSlice.tsx
+++ src/components/floofGPT/floofGPTSlice.tsx
@@ -13,7 +13,7 @@ export const askFloofGPT = createAsyncThunk(
         method: "GET",
         headers: { "Content-Type": "application/json", Accept: "application/json" },
         url: "https://api.levandolidze.me/floof",
-        data: JSON.stringify({ prompt: prompt }),
+        data: { prompt: prompt },
       })
 
       console.log(response)
