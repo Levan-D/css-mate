4294b1bcabf1f7de4143a26d13bb350cfeebeb5a
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-03-28 11:39:08 +0400

    1

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import "./animations.css"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
 
