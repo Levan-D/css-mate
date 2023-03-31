a72c16e22da5f0658d7f66b45ea043be4dea96c8
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-03-31 17:51:30 +0400

    Update InputText.tsx

--- src/features/colors/contrastChecker/contrastCheckerSidemenu/InputText.tsx
+++ src/features/colors/contrastChecker/contrastCheckerSidemenu/InputText.tsx
@@ -109,7 +109,7 @@ export default function InputText() {
           onClick={() => dispatch(toggleTextSize())}
           className={`btnSecondary `}
         >
-          {textSize ? "L" : "S"}
+          {textSize ? "L" : "N"}
         </div>
         <div
           onClick={() => setDisplaySwatchesOne(true)}
