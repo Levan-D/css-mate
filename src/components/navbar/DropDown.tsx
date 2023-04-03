48b3303f2d514af70912d580cd37d5564547fb1a
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-03 14:50:53 +0400

    1

--- src/components/navbar/DropDown.tsx
+++ src/components/navbar/DropDown.tsx
@@ -23,9 +23,9 @@ const DropDown = ({
       <ul
         className={` ${
           windowWidth > 540
-            ? pathArray[0] === category.catPath && "  block  sm:hidden"
-            : subMenuVis === category.catPath && "  !block  sm:hidden"
-        } static   hidden   translate-x-[0]  translate-y-[2px] rounded-lg  border-0  bg-none from-[#151f36]  to-slate-900 px-4 py-2 duration-200  sm:absolute sm:translate-x-[-30px] sm:border-2 sm:bg-gradient-to-r sm:group-hover:block`}
+            ? pathArray[0] === category.catPath && "  block  md:hidden"
+            : subMenuVis === category.catPath && "  !block  md:hidden"
+        } static   hidden   translate-x-[0]  translate-y-[2px] rounded-lg  border-0  bg-none from-[#151f36]  to-slate-900 px-4 py-2 duration-200  md:absolute md:translate-x-[-30px] md:border-2 md:bg-gradient-to-r md:group-hover:block`}
       >
         {category.catCon.map((btn, index) => (
           <li
@@ -34,7 +34,7 @@ const DropDown = ({
             className={`   ${
               windowWidth > 540 ? pathArray[1] === btn.path && " !bg-secondary-300" : ""
             } 
-            shrink-0 cursor-pointer select-none rounded-2xl   py-1 px-4 font-cursiveCustom text-base  font-bold duration-300 active:brightness-75 sm:text-lg sm:hover:bg-secondary-200 `}
+            shrink-0 cursor-pointer select-none rounded-2xl   py-1 px-4 font-cursiveCustom text-base  font-bold duration-300 active:brightness-75 md:text-lg md:hover:bg-secondary-200 `}
           >
             <Link className="p-1" to={category.catPath + "/" + btn.path}>
               {btn.name}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -42,12 +42,12 @@ const Navbar = () => {
       className={` ${
         !location.pathname.includes("/gradient-maker")
           ? "bg-primary"
-          : "bg-primary sm:bg-transparent"
-      }  z-40 h-fit     sm:h-16`}
+          : "bg-primary md:bg-transparent"
+      }  z-40 h-fit     md:h-16`}
     >
-      <nav className="mx-auto h-fit max-w-4xl items-center justify-between align-bottom sm:flex sm:h-16 sm:px-4">
+      <nav className="mx-auto h-fit max-w-4xl items-center justify-between align-bottom md:flex md:h-16 md:px-4">
         {/* home */}
-        <div className="mx-auto w-fit py-2 pl-8 sm:mx-0 sm:p-0">
+        <div className="mx-auto w-fit py-2 pl-8 md:mx-0 md:p-0 ">
           <Link to="/">
             <img
               className="h-12 cursor-pointer duration-200 active:brightness-75  "
@@ -58,17 +58,17 @@ const Navbar = () => {
         </div>
         {/* home */}
 
-        <ul className=" bg-slate-700 py-2 text-center sm:flex sm:bg-transparent sm:py-0 sm:text-start">
+        <ul className=" bg-slate-700 py-2 text-center md:flex md:bg-transparent md:py-0 md:text-start">
           {pageButtons.map((category, i) => (
             <li
               key={i}
               className={` ${
                 windowWidth > 540
                   ? pathArray[0] === category.catPath &&
-                    "   mx-4 !bg-slate-500 sm:mx-0 sm:!bg-secondary-300  "
+                    "   mx-4 !bg-slate-500 md:mx-0 md:!bg-secondary-300  "
                   : subMenuVis === category.catPath &&
-                    "   mx-4 !bg-slate-500 sm:mx-0 sm:!bg-secondary-300  "
-              }  group shrink-0 cursor-pointer select-none whitespace-nowrap rounded-2xl  px-4 py-1 font-cursiveCustom text-xl  font-bold duration-200  sm:hover:bg-secondary-200 `}
+                    "   mx-4 !bg-slate-500 md:mx-0 md:!bg-secondary-300  "
+              }  group shrink-0 cursor-pointer select-none whitespace-nowrap rounded-2xl  px-4 py-1 font-cursiveCustom text-xl  font-bold duration-200  md:hover:bg-secondary-200 `}
             >
               {windowWidth < 540 ? (
                 <div onClick={() => handleSubMenuClick(category.catPath)}>
@@ -92,7 +92,7 @@ const Navbar = () => {
           ))}
           {/* github link */}
           <li
-            className={` my-1 ml-4 hidden shrink-0  cursor-pointer select-none rounded-full border-2  border-transparent text-lg duration-300 active:brightness-75 sm:block sm:hover:border-white`}
+            className={` my-1 ml-4 hidden shrink-0  cursor-pointer select-none rounded-full border-2  border-transparent text-lg duration-300 active:brightness-75 md:block md:hover:border-white`}
           >
             <a href="https://github.com/Levan-D/CSSHomie" target="_blank">
               <img className="h-6" src={githubIcon} alt="" />
