130496fd54d83a9f9e5d8f260827fe694a50b4c5
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-17 21:28:50 +0400

    Revert "1"
    This reverts commit f03de3beae00551c123410d169b49aecd335b486.

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,17 @@
 /** @format */
 
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 import "./animations.css"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
 
-const rootElement = document.getElementById("root")
-
-if (rootElement) {
-  if (rootElement.hasChildNodes()) {
-    ReactDOM.hydrate(
-      <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </React.StrictMode>,
-      rootElement
-    )
-  } else {
-    ReactDOM.render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </React.StrictMode>,
-      rootElement
-    )
-  }
-} else {
-  console.error("Root element not found.")
-}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
