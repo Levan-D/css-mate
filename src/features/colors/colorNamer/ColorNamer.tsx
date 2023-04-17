bf78136824b46ca238a4f2c315757f54ce1c65ff
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-18 00:55:43 +0400

    3

--- src/features/colors/colorNamer/ColorNamer.tsx
+++ src/features/colors/colorNamer/ColorNamer.tsx
@@ -15,6 +15,16 @@ export default function ColorNamer() {
         />
 
         <meta name="keywords" content="color tool, color naming app, design tool" />
+        <meta property="og:title" content="Color Namer" />
+        <meta
+          property="og:description"
+          content="Discover the perfect color for your project with our color naming app.  Try our color naming app today and take your design to the next level."
+        />
+        <meta
+          property="og:image"
+          content={`${import.meta.env.BASE_URL}openGraphTags/colorNamer.png`}
+        />
+        <meta property="og:url" content="https://cssmate.online/colors/color-namer" />
       </Helmet>
       <h1 className="mb-8 text-center font-cursiveCustom   text-2xl">
         Name
