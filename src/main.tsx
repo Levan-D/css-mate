f03de3beae00551c123410d169b49aecd335b486
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-17 20:03:07 +0400

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,35 @@
 /** @format */
 
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
 import "./animations.css"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-)
+const rootElement = document.getElementById("root")
+
+if (rootElement) {
+  if (rootElement.hasChildNodes()) {
+    ReactDOM.hydrate(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+      rootElement
+    )
+  } else {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+      rootElement
+    )
+  }
+} else {
+  console.error("Root element not found.")
+}
