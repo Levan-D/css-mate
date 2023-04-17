cdfb8b64c12ca5ab152bc79695f3170daff85d19
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-17 21:31:41 +0400

    Revert "3"
    This reverts commit 6a6ae146c7e79aca5856b9a374ef1749753e1b9c.

--- src/features/colors/colorNamer/ColorNamer.tsx
+++ src/features/colors/colorNamer/ColorNamer.tsx
@@ -3,7 +3,6 @@
 import ColorNamerSidemenu from "./colorNamerSidemenu/ColorNamerSidemenu"
 //@ts-ignore
 import { Helmet } from "react-helmet"
-import colorNamer from "../../../assets/openGraphTags/colorNamer.png"
 
 export default function ColorNamer() {
   return (
@@ -14,15 +13,8 @@ export default function ColorNamer() {
           name="description"
           content="Discover the perfect color for your project with our color naming app.  Try our color naming app today and take your design to the next level."
         />
+
         <meta name="keywords" content="color tool, color naming app, design tool" />
-        <meta property="og:title" content="Color Namer" />
-        <meta
-          property="og:description"
-          content="Discover the perfect color for your project with our color naming app.  Try our color naming app today and take your design to the next level."
-        />
-        <meta property="og:image" content={colorNamer} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://cssmate.online/colors/color-namer" />
       </Helmet>
       <h1 className="mb-8 text-center font-cursiveCustom   text-2xl">
         Name
