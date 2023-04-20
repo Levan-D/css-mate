2f7093a0c4aa509d5463248e99e7b3fd4c8b807a
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-04-20 14:49:33 +0400

    3

--- prerender.cjs
+++ prerender.cjs
@@ -1,6 +1,6 @@
 /** @format */
 
-const pageButtons = require("./src/data/PageButtons").pageButtons
+const pageButtons = require("./src/data/pageButtonsCommon").pageButtons;
 const puppeteer = require("puppeteer")
 const fs = require("fs")
 const path = require("path")
---
+++ src/data/pageButtonsCommon.js
@@ -0,0 +1,5 @@
+import { pageButtons } from "./PageButtons";
+
+module.exports = {
+  pageButtons,
+};
